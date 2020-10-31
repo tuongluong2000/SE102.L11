d0c10c69df0d7970bf7fde6ae2216e70b0fa9ac6
tuongluong2000 <73729595+tuongluong2000@users.noreply.github.com>
2020-10-31 10:41:51 +0700

    Add files via upload

---
+++ 04-Collision/Brick.cpp
@@ -0,0 +1,21 @@
+#include "Brick.h"
+
+void CBrick::Render()
+{
+	animations[0]->Render(x, y);
+	//RenderBoundingBox();
+}
+
+void CBrick::Update(DWORD dt, vector<LPCBRICK>* coObjects)
+{
+	this->dt = dt;
+	dx = vx * dt;
+	dy = vy * dt;
+}
+void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
+{
+	l = x;
+	t = y;
+	r = x + BRICK_BBOX_WIDTH;
+	b = y + BRICK_BBOX_HEIGHT;
+}
